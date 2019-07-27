b366559fa515bb407270edeae3d9ea23a0a0d1d2
Anthonie <mrtizac@gmail.com>
2019-07-27 10:32:04 +0000

    Upload New File

---
+++ IMVUWealthBooster/Authentication.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TrinitySeal;
+
+namespace IMVUWealthBooster
+{
+    public partial class Authentication : Form
+    {
+        public Authentication()
+        {
+            InitializeComponent();
+        }
+
+        private void Authentication_Load(object sender, EventArgs e)
+        {
+            SealCheck.HashChecks();
+            if (SealCheck.isValidDLL)
+            {
+                Seal.InitializeForm("8S9UDWnRx5fIM0ARbQG8nVHkLSBEjz9ABNbOsP7LZuQ3j", "1.0", this, new Form1(), SealColours.Purple);
+                
+            }
+        }
+
+    }
+}
